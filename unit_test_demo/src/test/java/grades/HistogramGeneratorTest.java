a6e01a52bd6cb3a2f9aae8c8a91e2b9af1f504d4
kadirarslann <rslnkdr@gmail.com>
2020-03-17 21:47:58 +0300

    final commit

--- unit_test_demo/src/test/java/grades/HistogramGeneratorTest.java
+++ unit_test_demo/src/test/java/grades/HistogramGeneratorTest.java
@@ -10,16 +10,27 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
+import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.jfree.chart.JFreeChart;
 
+import org.jfree.chart.ChartFactory;
 public class HistogramGeneratorTest {
 
 	@Test
 	public void test_generate_normal() {
+       /* ChartIO ch = mock(ChartIO.class);
+        JFreeChart chart = mock(JFreeChart.class);
+        when(ch.readFile("")).thenReturn(new int[] {});
+		Mockito.doReturn(true).when(ch).writeFile(anyString(),any());
+        HistogramGenerator hg = new HistogramGenerator(ch);
+        hg
+        .generateHistogram("", "");*/
+		//????
 
 	}
 	@Test
@@ -32,6 +43,13 @@ public class HistogramGeneratorTest {
 	public void test2() {
 		HistogramGenerator his=new HistogramGenerator(new ChartIO());
 		Assert.assertTrue(his.createChart(new int[]{1,2,3,4,5,6,7}) !=null);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void test3() {
+		ChartIO io = mock(ChartIO.class);
+		HistogramGenerator his=new HistogramGenerator(io);
+		doThrow(new IllegalArgumentException()).when(io).writeFile("src",null);
+		his.exportChart("src",null);
 	}
 
 
